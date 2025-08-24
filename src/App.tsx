b4d52482b51431997769d18f4eb6846c3c5c91a1
Viktoriia <struk.victoria@gmail.com>
2025-08-24 22:10:24 +0300

    Del unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Portfolio from './Potfolio'
 
 function App() {
-
 
   return (
     <>
